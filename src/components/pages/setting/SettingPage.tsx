4053999dd5def22238640175e48cdef0e7d06c1f
jaks024 <ye7748560@gmail.com>
2022-09-04 21:42:38 -0400

    implemented google oauth

--- src/components/pages/setting/SettingPage.tsx
+++ src/components/pages/setting/SettingPage.tsx
@@ -1,14 +1,91 @@
 import { PageHeadBlock } from "../heading/PageHeadBlock";
+import { useGoogleOneTapLogin, GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+import { useEffect, useState } from "react";
 
 export function SettingPage() {
+
+    const REFRESH_TOKEN_COOKIE_NAME = "gis_refresh_token";
+    
+    const [accessToken, setAccessToken] = useState("");
+    const [refreshToken, setRefreshToken] = useState("");
+
+    const onLoadGetRefreshToken = () => {
+        const cookieRefreshToken = document.cookie.split('; ')
+            .find((row) => row.startsWith(`${REFRESH_TOKEN_COOKIE_NAME}=`))
+            ?.split('=')[1];
+        if (cookieRefreshToken === undefined || cookieRefreshToken.length == 0) {
+            return;
+        }
+        setRefreshToken(cookieRefreshToken);
+        onLoadGetAccessToken(cookieRefreshToken);
+    }
+
+    const onLoadGetAccessToken = (refreshToken: string) => {
+        fetch('http://localhost:3000/auth/access', {
+            method: 'GET',
+            headers: {
+                refreshToken: refreshToken
+            } 
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            setAccessToken(data);
+        });
+    }
+
+    useEffect(() => {
+        onLoadGetRefreshToken();
+    }, []);
+
+    const saveRefreshTokenToCookie = (token: string) => {
+        document.cookie = `gis_refresh_token=${token}`;
+    }
+
+    const onGoogleLoginSuccess = async (response: any) => {
+        fetch('http://localhost:3000/auth/google', {
+            method: 'GET',
+            headers: {
+                code: response.code
+            } 
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            setAccessToken(data.accessToken);
+            setRefreshToken(data.refreshToken);
+
+            saveRefreshTokenToCookie(data.refreshToken);
+        });
+    }
+
+    const onGoogleLoginErrorHandler = (response: any) => {
+        console.log('login failed');
+        console.log(response);
+    }
+
+    const googleLogin = useGoogleLogin({
+        flow: "auth-code",
+        scope: "https://www.googleapis.com/auth/drive.appdata",
+        onSuccess: onGoogleLoginSuccess,
+        onError: onGoogleLoginErrorHandler,
+    });
+
     return (
         <div className="page">
+            
             <PageHeadBlock 
                 name="Settings"
                 month=""
                 year=""
                 isRefreshButtonEnabled={false}
             />
+            <button onClick={() => googleLogin()}>Login to Google to Save!</button>
+            <div>
+                {refreshToken}
+            </div>
+            <div>
+                {accessToken}
+            </div>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider 
+                clientId="1019728199135-2bqo08s8e7ml30shimo019a4pqu275e1.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
