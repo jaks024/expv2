608578cb586514a24820565f1295ea9ffbf17c79
jaks024 <42505518+jaks024@users.noreply.github.com>
2022-09-08 18:06:42 -0400

    implemented add request to server

--- src/components/api/GWagonApiClient.ts
+++ src/components/api/GWagonApiClient.ts
@@ -1,18 +1,32 @@
 import { IEntry } from "../../interfaces/IEntry";
+import { userDataInstance } from "./GlobalUserData";
 
 export function GWagonApiClient() {
 
     const ROOT = "http://localhost:3000";
 
-    const addNewEntry = (newEntry: IEntry) => {
-        console.log("added ");
+    const addNewEntry = async (newEntry: IEntry) => {
+        console.log("post new entry ");
         console.log(JSON.stringify(newEntry));
-        console.log(newEntry);
+        const headers = new Headers();
+        headers.append("Accept", "application/json");
+        headers.append("Content-Type", "application/json");
+        headers.append("accessToken", userDataInstance.accessToken);
+
+        await fetch(`${ROOT}/add`, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(newEntry)
+        })
+        .then((res) => console.log(res))
+        .catch(err => {
+            console.log(err);
+        });
     }
 
     const getRefreshToken = (onGoogleLoginResponse: any, handler: (data: any) => void) => {
         fetch(`${ROOT}/auth/google`, {
-            method: 'GET',
+            method: "GET",
             headers: {
                 code: onGoogleLoginResponse.code
             } 
