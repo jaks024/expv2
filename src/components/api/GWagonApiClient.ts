0bb4f6dfb9209d7014c9e027cd3721d0af671a5f
jaks024 <ye7748560@gmail.com>
2022-09-11 18:54:12 -0400

    added firebase & updated cloud run api url

--- src/components/api/GWagonApiClient.ts
+++ src/components/api/GWagonApiClient.ts
@@ -7,7 +7,7 @@ import { userDataInstance } from "./GlobalUserData";
 
 export function GWagonApiClient() {
 
-    const ROOT = "http://localhost:3000";
+    const ROOT = "https://gwagon-tae44npnkq-pd.a.run.app";
 
     const addNewEntry = async (newEntry: IEntry) => {
         console.log("post new entry ");
